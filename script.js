129f9ea34f03ac4182b49b0487484a2cf7257086
Jonas <jonaspaneto@gmail.com>
2021-07-21 22:42:03 -0300

    Criação da função gameOver

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ let lightColor = (element, number) => {
 let checkOrder = () => {
     for(let i in clickedOrder) {
         if(clickedOrder[i] != order[i]) {
-            lose();
+            gameOver();
             break;
         }
     }
@@ -59,11 +59,60 @@ let checkOrder = () => {
 
 let click = (color) => {
     clickedOrder[clickedOrder.length] = color;
-    createColorElement(color).classList.add("selected");
+    createColorElement(color).classList.add('selected');
 
     setTimeout(() => {
-        createColorElement(color).classList.remove("selected");
-    })  
-
-    checkOrder();
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);    
 }
+
+//Função que retorna a cor
+
+let createColorElement = (color) => {
+    if(color == 0) {
+        return green;
+    } else if(color == 1) {
+        return red;
+    } else if(color == 2) {
+        return yellow;
+    }else if(color == 3) {
+        return blue;
+    }
+}
+
+//Função para proximo nivel do jogo
+
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+//Função para game over
+
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo`);
+    order = [];
+    clickedOrder = [];
+
+    playGame();
+}
+
+let playGame = () => {
+    alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+    score = 0;
+
+    nextLevel();
+}
+
+green.addEventListener('click', click(0));
+red.addEventListener("click", click(1));
+yellow.addEventListener("click", click(2));
+blue.addEventListener("click", click(3));
+
+playGame();
+
+
+
+
+
